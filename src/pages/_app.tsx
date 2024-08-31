bf396fad0e6778c5f7acae4c95e47cadfbcdd129
movingrock <movingrock99@gmail.com>
2024-08-31 15:52:37 +0900

    feat: nextjs project layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
+import GlobalLayout from "@/components/global-layout";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+import SearchableLayout from "@/components/searchable-layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <GlobalLayout>
+        <Component {...pageProps} />
+      </GlobalLayout>
+    </div>
+  );
 }
